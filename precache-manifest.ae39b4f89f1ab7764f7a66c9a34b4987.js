412897901c54e4757c04c491da26e95311b874f8
荣晓红 <44945323+xiaoixao@users.noreply.github.com>
2020-10-18 18:17:07 +0800

    Add files via upload

---
+++ precache-manifest.ae39b4f89f1ab7764f7a66c9a34b4987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a06ed6349bcf49f09bee147c61b35d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "318d38656875707b5ae0",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "31eb9c654c1805563980",
+    "url": "./static/css/main.f83598d1.chunk.css"
+  },
+  {
+    "revision": "318d38656875707b5ae0",
+    "url": "./static/js/2.ee2aa3ce.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.ee2aa3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31eb9c654c1805563980",
+    "url": "./static/js/main.a0598818.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
